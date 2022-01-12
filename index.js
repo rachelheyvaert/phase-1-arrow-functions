8de42a99cfa26815819d63680fd7310391423f4b
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-12 10:12:40 -0500

    commit tree 19653472b5a2c8ba76fdcd8143b41cf4de9abff6

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    console.log(divide);
+    return 2000 / 100
+}
+
+const square = number => {console.log(number)
+return number * number
+}
+
+const add = (part1, part2) => {console.log(part1, part2)
+return part1 + part2
+}
